79a4b1358a7e2c12984a098d0fb83e8f9daa5be8
Rahmat Riansyah <rahmat.riansyah@ist.id>
2025-03-10 10:28:39 +0700

    first flight 🚀

---
+++ app/data.py
@@ -0,0 +1,7 @@
+employee_list = [
+    {'id':1, 'nama':'Budi Santoso', 'nip':'123456', 'divisi':'IT'},
+    {'id':2, 'nama':'Aminah', 'nip':'654321', 'divisi':'IT'},
+    {'id':3, 'nama':'Marco', 'nip':'7890123', 'divisi':'IT'},
+    {'id':4, 'nama':'Bruno', 'nip':'223344', 'divisi':'IT'},
+    {'id':5, 'nama':'Clara', 'nip':'556633', 'divisi':'IT'}
+]
